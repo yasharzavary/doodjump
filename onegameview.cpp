3f8deb6a520c7c5f09502b07831e262e00bdef60
yasharzavary <zavaryyashar009@gmail.com>
2023-07-13 14:56:06 +0430

    pics added to the items touch part

--- onegameview.cpp
+++ onegameview.cpp
@@ -53,6 +53,7 @@ void oneGameView::controlStage()
         jetAdded=true;
     }
 
+//    control that doodler touch wich one of the stages
     for(int i=0; i<30; i++){
         if(stageList[i]->x() < gameDoodler->x()+40 && stageList[i]->x()+75 > gameDoodler->x()+45
             && stageList[i]->y()-10 < gameDoodler->y()+75 && stageList[i]->y()+4 > gameDoodler->y()+75 ){
@@ -60,23 +61,33 @@ void oneGameView::controlStage()
             gameDoodler->changeJumpForce(120);
         }
     }
+
+//    add shield and control that doodler touch the shield or not
     if(shieldAdded && shieldTemp->x() < gameDoodler->x()+40 && shieldTemp->x()+75 > gameDoodler->x()+40 &&
         shieldTemp->y()-5 < gameDoodler->y()+35 && shieldTemp->y()+50 > gameDoodler->y()+40)
         {
+        oneGameScene->removeItem(shieldTemp);
         shieldAdded=false;
-        std::cout<<"touch to the shield"<<std::endl;
+        gameDoodler->setPixmap(QPixmap(":/image/doodlerShield.png"));
     }
+
+//    add spring and control that doodler touch the spring or not
     if(springAdded && springTemp->x() < gameDoodler->x()+40 && springTemp->x()+75 > gameDoodler->x()+40 &&
-        springTemp->y()-5 < gameDoodler->y()+35 && springTemp->y()+50 > gameDoodler->y()+40)
+        springTemp->y()-5 < gameDoodler->y()+35 && springTemp->y()+8 > gameDoodler->y()+40)
     {
-        std::cout<<"touch to the spring"<<std::endl;
+        oneGameScene->removeItem(springTemp);
         springAdded=false;
     }
+
+//    add jet and control that doodler touch the jet or not
     if(jetAdded && jetTemp->x() < gameDoodler->x()+40 && jetTemp->x()+75 > gameDoodler->x()+40 &&
         jetTemp->y()-5 < gameDoodler->y()+35 && jetTemp->y()+50 > gameDoodler->y()+40)
     {
+        oneGameScene->removeItem(jetTemp);
+        gameDoodler->setPixmap(QPixmap(":/image/doodlerJet.png"));
         jetAdded=false;
     }
+
     if(gameDoodler->y() <= 0){
         gameDoodler->setPos(100,500);
         for(int i=0; i<30; i++){
